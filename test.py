3d23aa955f645a22bdde300e1291969f4b3f5b5e
Sibi <sibi@psibi.in>
2015-02-24 21:16:50 +0530

    Add another test case.

--- test.py
+++ test.py
@@ -66,6 +66,13 @@ class TestJsonCheck(unittest.TestCase):
         jdic = json.loads('[{"test1":3, "test2":4},{"test1":3, "test2":4}]')
         self.assertTrue(check_json_array(jdic, jobj))
 
+    def test_list_4f(self):
+        """Tests a list with object"""
+        jobj = JList({'parent': 'some', 'keys': [JObject({'parent': None, 'keys': ['test1', 'test2']}),
+                                                 JObject({'parent': None, 'keys': ['test1', 'test2']})]})
+        jdic = json.loads('[{"test1":3, "test2":4},{"test1":3, "test23":4}]')
+        self.assertFalse(check_json_array(jdic, jobj))
+
 if __name__ == '__main__':
     json_test = unittest.TestLoader().loadTestsFromTestCase(TestJsonCheck)
     testRunner = unittest.TextTestRunner()
