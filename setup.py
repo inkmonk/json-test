b9b7e6c2e19acf56d3eb77ef18183d821de792af
Sibi <sibi@psibi.in>
2015-02-26 17:30:38 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='json-test',
+      packages = find_packages(),
       version='0.1',
       description='A python library for assisting you in writing tests against a JSON structure.',
       url='https://github.com/inkmonk/json-test',
       author='Sibi',
       author_email='sibi@psibi.in',
       license='BSD3',
-      packages=['json-test'],
       zip_safe=False)
