170a2edd402528029f1ac6f5ef2a7a545befc77d
Sibi <sibi@psibi.in>
2015-02-26 17:17:49 +0530

    Fix syntax.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='json-test',
       version='0.1',
-      description='A python library for assisting you in writing tests against a JSON structure.'
+      description='A python library for assisting you in writing tests against a JSON structure.',
       url='https://github.com/inkmonk/json-test',
       author='Sibi',
       author_email='sibi@psibi.in',
